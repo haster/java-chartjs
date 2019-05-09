5f9ff14ba521e26da2ab1fbbdb0d3e719e914b2b
Sven Haster <buurman.sven@gmail.com>
2019-05-09 15:13:20 +0200

    First attempt at modularization, requires all the direct dependencies

--- data/src/main/java/module-info.java
+++ data/src/main/java/module-info.java
@@ -1,5 +1,13 @@
 module nl.crashdata.chartjs.data
 {
+	exports nl.crashdata.chartjs.data;
+
+	exports nl.crashdata.chartjs.colors;
+
+	exports nl.crashdata.chartjs.data.simple;
+
+	exports nl.crashdata.chartjs.data.simple.builder;
+
 	requires java.base;
 
 	requires com.fasterxml.jackson.annotation;
--- serialization/src/main/java/module-info.java
+++ serialization/src/main/java/module-info.java
@@ -1,5 +1,7 @@
 module nl.crashdata.chartjs.serialization
 {
+	exports nl.crashdata.chartjs.serialization;
+
 	requires java.base;
 
 	requires com.fasterxml.jackson.annotation;
@@ -9,4 +11,6 @@ module nl.crashdata.chartjs.serialization
 	requires com.fasterxml.jackson.databind;
 
 	requires com.fasterxml.jackson.datatype.jsr310;
+
+	requires nl.crashdata.chartjs.data;
 }
