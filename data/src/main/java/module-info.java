e7dfcc558d5c53ffd9b83683c9f217a66732aab6
Sven Haster <buurman.sven@gmail.com>
2019-05-09 11:52:39 +0200

    Added module descriptors for data and serializable
    All their dependencies have Automatic-Module-Name's at the very least

---
+++ data/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module nl.crashdata.chartjs.data
+{
+	requires java.base;
+
+	requires com.fasterxml.jackson.annotation;
+}
---
+++ serialization/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module nl.crashdata.chartjs.serialization
+{
+	requires java.base;
+
+	requires com.fasterxml.jackson.annotation;
+
+	requires com.fasterxml.jackson.core;
+
+	requires com.fasterxml.jackson.databind;
+
+	requires com.fasterxml.jackson.datatype.jsr310;
+}
