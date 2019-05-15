a05a85534a7e0e7a05b4ecb4ae1438a555b07116
Sven Haster <buurman.sven@gmail.com>
2019-05-15 15:12:02 +0200

    Don't depend on skyscreamer
    This artifact splits the org.json package over itself and its direct dependency.
    Now we can run tests without using a module-info

--- serialization/src/test/java/nl/crashdata/chartjs/serialization/test/ChartJsDataTest.java
+++ serialization/src/test/java/nl/crashdata/chartjs/serialization/test/ChartJsDataTest.java
@@ -1,5 +1,7 @@
 package nl.crashdata.chartjs.serialization.test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,13 +13,15 @@ import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import nl.crashdata.chartjs.colors.ChartJsRGBAColor;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import nl.crashdata.chartjs.data.ChartJsBoundaryType;
 import nl.crashdata.chartjs.data.ChartJsFill;
 import nl.crashdata.chartjs.data.ChartJsInteractionMode;
 import nl.crashdata.chartjs.data.ChartJsTimeUnit;
+import nl.crashdata.chartjs.data.colors.ChartJsRGBAColor;
 import nl.crashdata.chartjs.data.simple.SimpleChartJsXYDataPoint;
 import nl.crashdata.chartjs.data.simple.builder.SimpleChartJsConfigBuilder;
 import nl.crashdata.chartjs.data.simple.builder.SimpleChartJsLinearAxisConfigBuilder;
@@ -25,15 +29,12 @@ import nl.crashdata.chartjs.data.simple.builder.SimpleChartJsLocalDateAxisConfig
 import nl.crashdata.chartjs.data.simple.builder.SimpleChartJsOptionsBuilder;
 import nl.crashdata.chartjs.serialization.ChartJsObjectMapperFactory;
 import nl.crashdata.chartjs.serialization.test.resources.TestResourcesMarker;
-import org.json.JSONException;
 import org.junit.jupiter.api.Test;
-import org.skyscreamer.jsonassert.JSONAssert;
-import org.skyscreamer.jsonassert.JSONCompareMode;
 
 public class ChartJsDataTest
 {
 	@Test
-	public void basicObjectMapping() throws JSONException, IOException
+	public void basicObjectMapping() throws IOException
 	{
 		SimpleChartJsConfigBuilder<SimpleChartJsXYDataPoint<LocalDate, Integer>> config =
 			SimpleChartJsConfigBuilder.lineChart();
@@ -69,12 +70,18 @@ public class ChartJsDataTest
 	}
 
 	private void assertOutputMatches(Serializable objectToMap, String expectedOutput)
-			throws JsonProcessingException, JSONException
+			throws IOException
 	{
-		ObjectMapper mapper = ChartJsObjectMapperFactory.getObjectMapper(true);
+		ObjectMapper chartjsMapper = ChartJsObjectMapperFactory.getObjectMapper(true);
 
-		JSONAssert.assertEquals(expectedOutput, mapper.writeValueAsString(objectToMap),
-			JSONCompareMode.STRICT);
+		ObjectMapper defaultMapper = new ObjectMapper();
+		defaultMapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+		defaultMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+
+		JsonNode actual = defaultMapper.readTree(chartjsMapper.writeValueAsString(objectToMap));
+		JsonNode expected = defaultMapper.readTree(expectedOutput);
+
+		assertEquals(expected, actual);
 	}
 
 	private static SortedMap<LocalDate, Integer> createUserCountMap()
